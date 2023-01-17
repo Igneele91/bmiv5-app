7c87cdaaf8aa814dec20816ef7c514c6f182e259
Igneele <69846931+Igneele91@users.noreply.github.com>
2023-01-17 19:43:19 +0100

    org backup safe right
    Absicherung

--- BMI-Calculator-App-master/lib/main.dart
+++ BMI-Calculator-App-master/lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, // eckenbanner weg
       theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
